50a1eca9d212e0f1201b30b4f6258581018f824d
Cameron Cardinal <cameriancardinal@gmail.com>
2024-03-26 16:52:13 +0100

    dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,8 +13,8 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
+        // return parent::index();
+        return $this->render('admin/dashboard/index.html.twig');
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
