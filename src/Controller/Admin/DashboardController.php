f4bfcd53d943bae647fe0063dae5e02cf5c5e595
Cameron Cardinal <cameriancardinal@gmail.com>
2024-04-24 15:48:07 +0200

    added user crud + role_author + hasher on creation

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -47,31 +47,43 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        return [
-            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+
+        yield MenuItem::linkToRoute('Retour sur le site', 'fa fa-undo', 'app_home');
+
+        yield MenuItem::linkToLogout('Déconnexion', 'fas fa-right-from-bracket');
             
-            MenuItem::section('Articles'),
-            MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
+        if($this->isGranted('ROLE_AUTHOR')) {
+            yield MenuItem::section('Articles');
+            yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
                 MenuItem::linkToCrud('Tous les articles', 'fas fa-list', Article::class),
                 MenuItem::linkToCrud('Ajout article', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
-            ]),
-            MenuItem::subMenu('Médias', 'fas fa-image')->setSubItems([
+                ]);
+            yield MenuItem::subMenu('Médias', 'fas fa-image')->setSubItems([
                 MenuItem::linkToCrud('Tous les médias', 'fas fa-list', Media::class),
                 MenuItem::linkToCrud('Ajout média', 'fas fa-plus', Media::class)->setAction(Crud::PAGE_NEW),
-            ]),
+            ]);
+        }
+        
 
-            MenuItem::section('Pages'),
-            MenuItem::subMenu('Pages', 'fas fa-pen-nib')->setSubItems([
-                MenuItem::linkToCrud('Toutes les pages', 'fas fa-list', Page::class),
-                MenuItem::linkToCrud('Ajout page', 'fas fa-plus', Page::class)->setAction(Crud::PAGE_NEW),
-            ]),
-            MenuItem::subMenu('Miniatures', 'fas fa-image')->setSubItems([
-                MenuItem::linkToCrud('Toutes les miniatures', 'fas fa-list', Miniature::class),
-                MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Miniature::class)->setAction(Crud::PAGE_NEW),
-            ]),
+            if ($this->isGranted('ROLE_ADMIN')) {
+                yield MenuItem::section('Pages');
+                yield MenuItem::subMenu('Pages', 'fas fa-pen-nib')->setSubItems([
+                    MenuItem::linkToCrud('Toutes les pages', 'fas fa-list', Page::class),
+                    MenuItem::linkToCrud('Ajout page', 'fas fa-plus', Page::class)->setAction(Crud::PAGE_NEW),
+                ]);
+                yield MenuItem::subMenu('Miniatures', 'fas fa-image')->setSubItems([
+                    MenuItem::linkToCrud('Toutes les miniatures', 'fas fa-list', Miniature::class),
+                    MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Miniature::class)->setAction(Crud::PAGE_NEW),
+                ]);
+            }
 
-            MenuItem::section('Users'),
-            MenuItem::linkToCrud('Users', 'fas fa-user', User::class),
-        ];
+            if ($this->isGranted('ROLE_ADMIN')) {
+                yield MenuItem::section('Users');
+                yield MenuItem::subMenu('Users', 'fas fa-user')->setSubItems([
+                    MenuItem::linkToCrud('Tous les users', 'fas fa-list', User::class),
+                    MenuItem::linkToCrud('Ajouter', 'fas fa-plus', User::class)->setAction(Crud::PAGE_NEW),
+                ]);
+            };
     }
 }
