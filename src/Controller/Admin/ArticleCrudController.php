ca39d665aff9707094efd05b82dd577e9c7c1fce
Cameron Cardinal <cameriancardinal@gmail.com>
2024-04-25 11:20:59 +0200

    added media for articles

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
@@ -24,6 +25,7 @@ class ArticleCrudController extends AbstractCrudController
         yield TextEditorField::new('content');
         yield TextField::new('category');
         yield TextField::new('featuredText');
+        yield AssociationField::new('featuredImage');
         yield DateTimeField::new('createdAt')->hideOnForm();
         yield DateTimeField::new('updatedAt')->hideOnForm();
     }
--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -62,4 +62,9 @@ class Media
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
