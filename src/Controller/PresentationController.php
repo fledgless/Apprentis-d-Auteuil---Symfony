7ace2a7e378ba28c12bc9deb5e24ea9c119d80ac
Cameron Cardinal <cameriancardinal@gmail.com>
2024-04-26 10:56:05 +0200

    tested list on présentation

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\PageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PresentationController extends AbstractController
 {
-    #[Route('/presentation', name: 'app_presentation')]
-    public function index(): Response
+    #[Route('/presentation', name: 'app_actualites')]
+    public function index(PageRepository $pageRepo): Response
     {
         return $this->render('presentation/index.html.twig', [
-            'controller_name' => 'PresentationController',
+            'pages' => $pageRepo->findBy(['category' => 'Présentation']),
         ]);
     }
 }
