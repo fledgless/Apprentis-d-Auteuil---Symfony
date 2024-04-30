1a7c20fa5ee8698c015756d93598a8debdd0ce13
Cameron Cardinal <cameriancardinal@gmail.com>
2024-04-30 10:55:50 +0200

    added all pages in un projet professionnel, changed some routes

--- src/Controller/UnEcrinVertController.php
+++ src/Controller/UnEcrinVertController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class UnEcrinVertController extends AbstractController
 {
     #[Route('/un-ecrin-vert', name: 'app_un_ecrin_vert')]
-    public function index(): Response
+    public function index(PageRepository $pageRepo): Response
     {
         return $this->render('un_ecrin_vert/index.html.twig', [
-            'controller_name' => 'UnEcrinVertController',
+            'pages' => $pageRepo->findBy(['category' => 'Un écrin vert']),
         ]);
     }
 }
