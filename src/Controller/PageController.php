7f47ee4a1ec5d8401b84460025382a8ed1760f8a
Cameron Cardinal <cameriancardinal@gmail.com>
2024-05-01 21:40:29 +0200

    added page show + css for page + change page_show route with category

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\PageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/page/{slug}', name: 'page_show')]
-    public function show(): Response
+    #[Route('/{category}/{slug}', name: 'page_show', methods: ['GET'])]
+    public function show(string $slug, PageRepository $pageRepo): Response
     {
+        $page = $pageRepo->findOneBy(['slug' => $slug]);
         return $this->render('page/show.html.twig', [
-            'controller_name' => 'PageController',
+            'page' => $page
         ]);
     }
 }
