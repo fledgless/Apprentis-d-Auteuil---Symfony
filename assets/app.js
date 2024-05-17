bf069845442fc280ee35521abf4b391c8b507e55
Cameron Cardinal <cameriancardinal@gmail.com>
2024-05-17 10:50:54 +0200

    added function to validate form (can't link to form) + jsdoc

--- assets/app.js
+++ assets/app.js
@@ -43,3 +43,47 @@ nextButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft -= slideWidth;
   });
+
+// fonction pour vérifier le formulaire de contact
+/**
+ * @returns {boolean} 
+ * @description Recuperate all fields from form as variables and define a condition and an error message to push.
+ */
+function validateFormContact() { 
+    // récupération des forms
+    let name = document.querySelector("#name").value; 
+    let subject = document.querySelector("#subject").value; 
+    let email = document.querySelector("#email").value; 
+    let message = document.querySelector("#message").value; 
+    let error_message = document.querySelector("#error_message"); 
+    error_message.style.padding = "10px"; 
+  
+    let errors = []; 
+
+    // vérification nom
+    if (name.length < 3) { 
+        errors.push("Veuillez entrer un nom valide.");
+    } 
+    // vérification du mail
+    if (email.indexOf("@") === -1 || email.length < 6) { 
+        errors.push("Veuillez entrer une adresse mail valide.");
+    } 
+    // vérification sujet
+    if (subject.length < 5) { 
+        errors.push("Veuillez entrer un sujet valide.");
+    }
+    // vérification du message
+    if (message.length <= 30) { 
+        errors.push("Veuillez écrire un message de plus de 30 caractères.");
+    } 
+    // envoi du message en fonction des erreurs
+    if (errors.length > 0) { 
+        error_message.textContent = errors.join(" "); 
+        return false;
+    } 
+    else { 
+        alert( 
+            "Votre message a été envoyé !"); 
+        return true;
+    }
+}
